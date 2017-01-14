5071267026c1cb52dcb8b2ae664d0d76e7cee0df
Juanan <juan.antonadelval@gmail.com>
2017-01-14 12:30:48 +0100

    Set dust engine

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('dust', require('consolidate').dust);
+app.set('view engine', 'dust');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
